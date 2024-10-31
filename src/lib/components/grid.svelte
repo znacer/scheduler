ada3182245c03a9f0b82eb7879d54131e8315613
Zakaria Nacer <zak.nacer@hotmail.fr>
2024-10-31 20:47:42 +0100

    remove console log

--- src/lib/components/grid.svelte
+++ src/lib/components/grid.svelte
@@ -5,41 +5,40 @@
   import GridChunk from "$lib/components/gridChunck.svelte";
   import GridChunckAxis from "$lib/components/chucks/gridChunckAxis.svelte";
   import GridLabels from "./gridLabels.svelte";
-  $inspect(grid_layout_store.zoom);
 </script>
 
-  <div class="flex flex-row w-full h-fit">
-    <GridLabels />
-    <ScrollArea class="w-full" orientation="horizontal">
-      <div class="sticky">
-        {#if grid_layout_store.zoom == Zoom.DAY}
-          <div style="height: {grid_layout_store.axis_height}px;">
-            <GridChunckAxis zoom={Zoom.WEEK} />
-          </div>
-          <div style="height: {grid_layout_store.axis_height}px;">
-            <GridChunckAxis zoom={Zoom.DAY} week_day={true} />
-          </div>
-          <div style="height: {grid_layout_store.axis_height}px;">
-            <GridChunckAxis zoom={Zoom.DAY} />
-          </div>
-        {:else if grid_layout_store.zoom == Zoom.HOUR}
-          <div style="height: {grid_layout_store.axis_height}px;">
-            <GridChunckAxis zoom={Zoom.DAY} />
-          </div>
-          <div style="height: {grid_layout_store.axis_height}px;">
-            <GridChunckAxis utc={true} />
-          </div>
-          <div style="height: {grid_layout_store.axis_height}px;">
-            <GridChunckAxis utc={false} />
-          </div>
+<div class="flex flex-row w-full h-fit">
+  <GridLabels />
+  <ScrollArea class="w-full" orientation="horizontal">
+    <div class="sticky">
+      {#if grid_layout_store.zoom == Zoom.DAY}
+        <div style="height: {grid_layout_store.axis_height}px;">
+          <GridChunckAxis zoom={Zoom.WEEK} />
+        </div>
+        <div style="height: {grid_layout_store.axis_height}px;">
+          <GridChunckAxis zoom={Zoom.DAY} week_day={true} />
+        </div>
+        <div style="height: {grid_layout_store.axis_height}px;">
+          <GridChunckAxis zoom={Zoom.DAY} />
+        </div>
+      {:else if grid_layout_store.zoom == Zoom.HOUR}
+        <div style="height: {grid_layout_store.axis_height}px;">
+          <GridChunckAxis zoom={Zoom.DAY} />
+        </div>
+        <div style="height: {grid_layout_store.axis_height}px;">
+          <GridChunckAxis utc={true} />
+        </div>
+        <div style="height: {grid_layout_store.axis_height}px;">
+          <GridChunckAxis utc={false} />
+        </div>
+      {/if}
+    </div>
+    <div class="relative h-fit">
+      {#each schedules_store.are_checked() as [k, v]}
+        {#if v}
+          <GridChunk schedule_id={k} />
         {/if}
-      </div>
-      <div class="relative h-fit">
-        {#each schedules_store.are_checked() as [k, v]}
-          {#if v}
-            <GridChunk schedule_id={k} />
-          {/if}
-        {/each}
-      </div>
-    </ScrollArea>
-  </div>
+      {/each}
+    </div>
+  </ScrollArea>
+</div>
