a03c5f4502d5278e53f411e83059b4870bc53eb2
Zakaria Nacer <zak.nacer@hotmail.fr>
2024-11-04 15:32:16 +0100

    styling

--- src/lib/components/taskForm.svelte
+++ src/lib/components/taskForm.svelte
@@ -1,16 +1,16 @@
 <script lang="ts">
-    import * as Select from "$lib/components/ui/select/index";
-    import { colorHashMap, ColorPalette } from "$lib/stores/schedules.svelte";
-    import { type Task } from "$lib/stores/tasks.svelte";
-    import Datetimepicker from "./datetimepicker.svelte";
-    import Durationpicker from "./durationpicker.svelte";
-    import Input from "./ui/input/input.svelte";
-    import Label from "./ui/label/label.svelte";
+  import * as Select from "$lib/components/ui/select/index";
+  import { colorHashMap } from "$lib/stores/schedules.svelte";
+  import { type Task } from "$lib/stores/tasks.svelte";
+  import Datetimepicker from "./datetimepicker.svelte";
+  import Durationpicker from "./durationpicker.svelte";
+  import Input from "./ui/input/input.svelte";
+  import Label from "./ui/label/label.svelte";
 
   interface TaskFormProps {
-    form_values: Task
+    form_values: Task;
   }
-  let { form_values = $bindable()}: TaskFormProps = $props();
+  let { form_values = $bindable() }: TaskFormProps = $props();
 
   let task_end = $state(form_values.start + form_values.duration);
   $effect(() => {
@@ -21,42 +21,34 @@
   });
 </script>
 
-    <div class="flex flex-col gap-4 py-4">
-      <div class="flex flex-row items-center gap-4">
-        <Label for="name" class="text-right">Name</Label>
-        <Input id="name" bind:value={form_values.name} class="col-span-3" />
-      </div>
-      <div class="flex flex-row items-center gap-4">
-        <Label for="start" class="text-right">Début</Label>
-        <Datetimepicker bind:timestamp={form_values.start} />
-      </div>
-      <div class="flex flex-row items-center gap-4">
-        <Label for="duration" class="text-right">Durée</Label>
-        <Durationpicker bind:duration={form_values.duration}/>
-      </div>
-      <div class="flex flex-row items-center gap-4">
-        <Label for="end" class="text-right">Fin</Label>
-        <Datetimepicker bind:timestamp={task_end} />
-      </div>
-      <div class="flex flex-row items-center gap-4">
-        <Label for="color" class="text-right">Couleur</Label>
-        <Select.Root
-          onSelectedChange={(e) => {
-            if (e !== undefined) {
-              form_values.color = e.value as ColorPalette
-            }
-          }}
-        >
-          <Select.Trigger>
-            <Select.Value placeholder={form_values.color} />
-          </Select.Trigger>
-          <Select.Content>
-            <Select.Group>
-              {#each colorHashMap as [k, color]}
-                <Select.Item value={color}>{k}</Select.Item>
-              {/each}
-            </Select.Group>
-          </Select.Content>
-        </Select.Root>
-      </div>
-    </div>
+<div class="flex flex-col gap-4 py-4">
+  <div class="flex flex-row items-center gap-4">
+    <Label for="name" class="text-right">Name</Label>
+    <Input id="name" bind:value={form_values.name} class="col-span-3" />
+  </div>
+  <div class="flex flex-row items-center gap-4">
+    <Label for="start" class="text-right">Début</Label>
+    <Datetimepicker bind:timestamp={form_values.start} />
+  </div>
+  <div class="flex flex-row items-center gap-4">
+    <Label for="duration" class="text-right">Durée</Label>
+    <Durationpicker bind:duration={form_values.duration} />
+  </div>
+  <div class="flex flex-row items-center gap-4">
+    <Label for="end" class="text-right">Fin</Label>
+    <Datetimepicker bind:timestamp={task_end} />
+  </div>
+  <div class="flex flex-row items-center gap-4">
+    <Label for="color" class="text-right">Couleur</Label>
+    <Select.Root bind:value={form_values.color} type="single">
+      <Select.Trigger>{form_values.color}</Select.Trigger>
+      <Select.Content>
+        <Select.Group>
+          {#each colorHashMap as [k, color]}
+            <Select.Item value={color}>{k}</Select.Item>
+          {/each}
+        </Select.Group>
+      </Select.Content>
+    </Select.Root>
+  </div>
+</div>
