3a68659371ec355354a7d45f1500c5698a2cfed9
Zakaria Nacer <zak.nacer@hotmail.fr>
2024-11-13 19:53:09 +0100

    Update transitions

--- src/lib/components/chucks/chunck-label.svelte
+++ src/lib/components/chucks/chunck-label.svelte
@@ -2,15 +2,15 @@
   import type { Snippet } from "svelte";
   import { scale } from "svelte/transition";
 
-  let { children, height }: {children: Snippet, height: number} = $props()
+  let { children, height }: { children: Snippet; height: number } = $props();
 </script>
 
 <div
   class="sticky w-full h-full content-center left-0 z-10 border border-primary"
   style="height: {height}px;"
-  transition:scale|global = {{
+  in:scale|global={{
     delay: 100,
-    duration: 1000
+    duration: 1000,
   }}
 >
   {@render children?.()}
